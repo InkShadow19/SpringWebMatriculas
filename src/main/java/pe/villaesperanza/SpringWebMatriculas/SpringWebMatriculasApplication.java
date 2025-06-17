bef3b870ef7bb1a4438fb72b33f5a4ee44db4cd4
InkShadow19 <SM74726725@idat.edu.pe>
2025-06-16 23:31:59 -0500

    first commit

---
+++ src/main/java/pe/villaesperanza/SpringWebMatriculas/SpringWebMatriculasApplication.java
@@ -0,0 +1,13 @@
+package pe.villaesperanza.SpringWebMatriculas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebMatriculasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebMatriculasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/villaesperanza/SpringWebMatriculas/SpringWebMatriculasApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.villaesperanza.SpringWebMatriculas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebMatriculasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
