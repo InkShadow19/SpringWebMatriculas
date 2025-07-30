710be0e025e352797841771ea6837d3b9b9f0a7c
InkShadow19 <SM74726725@idat.edu.pe>
2025-07-29 23:53:00 -0500

    Versión 1.3.1

--- src/main/java/pe/villaesperanza/SpringWebMatriculas/service/EmailService.java
+++ src/main/java/pe/villaesperanza/SpringWebMatriculas/service/EmailService.java
@@ -14,13 +14,13 @@ public class EmailService {
     public void sendPasswordResetNotification(String username) {
         SimpleMailMessage message = new SimpleMailMessage();
         // Cambia este correo por el del administrador que recibirá la notificación
-        message.setTo("ivantexsebastian@gmail.com"); 
+        message.setTo("percyfuertesanaya@gmail.com");
         message.setSubject("Solicitud de Restablecimiento de Contraseña");
         message.setText("El usuario '" + username + "' ha solicitado un restablecimiento de su contraseña.\n\n"
                       + "Por favor, contacta al usuario y asígnale una nueva contraseña desde el módulo de gestión de usuarios.");
         
         // El correo desde el que se envía (debe ser el mismo que configuraste en application.properties)
-        message.setFrom("ivantexsilva@gmail.com");
+        message.setFrom("soportematriculasve@gmail.com");
 
         try {
             mailSender.send(message);
